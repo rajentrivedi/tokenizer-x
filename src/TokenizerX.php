6c95db5a160f11d59e8b3b8abca85d9c6161256a
Rajen Trivedi <rajen.trivedi@indianic.com>
2023-12-05 10:56:10 +0530

    added support for gpt-4

--- src/TokenizerX.php
+++ src/TokenizerX.php
@@ -2,17 +2,33 @@
 
 namespace Rajentrivedi\TokenizerX;
 
+use Yethee\Tiktoken\EncoderProvider;
+
 class TokenizerX
 {
-    public static function tokens($prompt): array
+    public static function tokens($prompt, $model = null): array
     {
+        if($model != null)
+        {
+            $provider = new EncoderProvider();
+            $encoder = $provider->getForModel($model);
+            $tokens = $encoder->encode($prompt);
+            return $tokens;
+        }
         $calculator = new TokenCalculator();
 
         return $calculator->gpt_encode($prompt);
     }
 
-    public static function count($prompt): int
+    public static function count($prompt, $model = null): int
     {
+        if($model != null)
+        {
+            $provider = new EncoderProvider();
+            $encoder = $provider->getForModel($model);
+            $tokens = $encoder->encode($prompt);
+            return count($tokens);
+        }
         $calculator = new TokenCalculator();
 
         return count($calculator->gpt_encode($prompt));
